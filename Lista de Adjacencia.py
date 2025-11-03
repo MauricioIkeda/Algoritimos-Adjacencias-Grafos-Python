97751e18b8a8d943a284b4f6258ce153ef0bfe7e
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2025-11-03 20:56:59 -0300

    Melhorias e correções de bugs!

--- Lista de Adjacencia.py
+++ Lista de Adjacencia.py
@@ -1,5 +1,5 @@
 class Grafo():
-    def __init__(self, vertice, direcional):
+    def __init__(self, vertice : str, direcional : bool):
         self.ListaAdjacencia = {vertice: []}
         self.direcional = direcional
 
@@ -9,17 +9,27 @@ class Grafo():
             print(f"{vertice} - {self.ListaAdjacencia.get(vertice)}")
         print()
 
-    def AddVertice(self, vertice):
+    def AddVertice(self, vertice : str):
         self.ListaAdjacencia[vertice] = []
-    
-    def AddAresta(self, vertice1, vertice2):
+
+    def AddAresta(self, vertice1 : str, vertice2 : str):
+        if vertice1 not in self.ListaAdjacencia.keys():
+            self.AddVertice(vertice1)
+
+        if vertice2 not in self.ListaAdjacencia.keys():
+            self.AddVertice(vertice2)
+
         self.ListaAdjacencia[vertice1].append(vertice2)
 
         if self.direcional:
             self.ListaAdjacencia[vertice2].append(vertice1)
 
-    def RemoverVertice(self, verticeRemover):
+    def RemoverVertice(self, verticeRemover : str):
         self.ListaAdjacencia.pop(verticeRemover)
+
+        for vertice in self.ListaAdjacencia.keys():
+            if verticeRemover in self.ListaAdjacencia[vertice]:
+                self.ListaAdjacencia[vertice].pop(self.ListaAdjacencia[vertice].index(verticeRemover))
 
         if self.direcional:
             for vertice in self.ListaAdjacencia.keys():
@@ -28,7 +38,7 @@ class Grafo():
         
         print(f"Vertice {verticeRemover} exluido com sucesso!\n")
 
-    def RemoverAresta(self, aresta):
+    def RemoverAresta(self, aresta : list[str]):
         self.ListaAdjacencia[aresta[0]].remove(aresta[1])
 
         if self.direcional:
@@ -37,22 +47,38 @@ class Grafo():
         print(f"Aresta {aresta} removida com sucesso!\n")
 
     def CalcularGrau(self):
+        grauSaida = 0
+        grauEntrada = 0
+
+        print("Grau de saida dos vertices:\n")
         for vertice in self.ListaAdjacencia.keys():
-            print(f"Vertice {vertice} tem grau {len(self.ListaAdjacencia[vertice])}\n")
-    
-    def VerificarAresta(self, vertice1, vertice2):
+            print(f"Vertice {vertice} tem grau: {len(self.ListaAdjacencia[vertice])}\n")
+            grauSaida += len(self.ListaAdjacencia[vertice])
+        
+        print("Grau de entrada dos vertices:\n")
+        for vertice in self.ListaAdjacencia.keys():
+            grauEntradaVertice = 0
+            for v in self.ListaAdjacencia.keys():
+                if vertice in self.ListaAdjacencia[v]:
+                    grauEntradaVertice += 1
+            print(f"Vertice {vertice} tem grau: {grauEntradaVertice}\n")
+            grauEntrada += grauEntradaVertice
+
+        print(f"Grau de total: {grauEntrada + grauSaida}\n")
+
+    def VerificarAresta(self, vertice1 : str, vertice2 : str):
         if vertice2 in self.ListaAdjacencia[vertice1]:
             print(f"Existe uma aresta {vertice1, vertice2}\n")
         else:
             print(f"Não existe a aresta {vertice1, vertice2}\n")
-    
-    def ListarVizinhoVertice(self, vertice):
+
+    def ListarVizinhoVertice(self, vertice : str):
         if vertice in self.ListaAdjacencia:
             print(f"Os vizinhos do vertice {vertice} são: {self.ListaAdjacencia[vertice]}\n")
         else:
             print(f"Não existe o vertice {vertice}")
 
-    def VerificarPercurso(self, percurso):
+    def VerificarPercurso(self, percurso : list[str]):
         for index, vertice in enumerate(percurso):
             if(len(percurso) != index + 1):
                 if percurso[index + 1] not in self.ListaAdjacencia[vertice]:
@@ -78,8 +104,6 @@ grafo.RemoverVertice("D")
 
 grafo.VerificarAresta("C", "D")
 
-grafo.CalcularGrau()
-
 grafo.VerificarAresta("A", "B")
 grafo.VerificarAresta("A", "C")
 
@@ -95,3 +119,5 @@ grafo.RemoverAresta(["A", "B"])
 grafo.VerificarAresta("A", "B")
 
 grafo.ExibirGrafos()
+
+grafo.CalcularGrau()
